820fe44e7a83282a6f234c04eba4b1ca3232629e
Dhruv Bhavsar <40565844+dbhavsar76@users.noreply.github.com>
2025-12-27 18:51:54 +0530

    update SettingsUserInterfaceScreen with preference elements

--- app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsUserInterfaceScreen.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsUserInterfaceScreen.kt
@@ -2,9 +2,15 @@ package com.nutomic.syncthingandroid.settings
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.navigation3.runtime.EntryProviderScope
 import com.nutomic.syncthingandroid.R
+import me.zhanghai.compose.preference.ListPreference
+import me.zhanghai.compose.preference.SwitchPreference
 
 
 fun EntryProviderScope<SettingsRoute>.settingsUserInterfaceEntry() {
@@ -16,9 +22,31 @@ fun EntryProviderScope<SettingsRoute>.settingsUserInterfaceEntry() {
 
 @Composable
 fun SettingsUserInterfaceScreen() {
+    val themeNames = stringArrayResource(R.array.app_theme_names)
+    val themeValues = stringArrayResource(R.array.app_theme_values)
+
+    val theme = remember { mutableStateOf(themeValues[0]) }
+    val expertMode = remember { mutableStateOf(false) }
+    val startInWebGui = remember { mutableStateOf(false) }
+
     SettingsScaffold(
         title = stringResource(R.string.category_user_interface),
     ) {
-        Text(stringResource(R.string.category_user_interface))
+        ListPreference(
+            title = { Text(stringResource(R.string.preference_app_theme_title)) },
+            state = theme,
+            values = themeValues.toList(),
+            valueToText = { value -> buildAnnotatedString { append(themeNames[themeValues.indexOf(value)]) } }
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.expert_mode_title)) },
+            summary = { Text(stringResource(R.string.expert_mode_summary)) },
+            state = expertMode,
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.start_into_web_gui_title)) },
+            summary = { Text(stringResource(R.string.start_into_web_gui_summary)) },
+            state = startInWebGui,
+        )
     }
 }
