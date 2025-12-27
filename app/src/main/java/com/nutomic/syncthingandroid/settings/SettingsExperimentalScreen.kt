c7abe7b1bf22f8d7e7e3544c1093260c0f1e20c1
Dhruv Bhavsar <40565844+dbhavsar76@users.noreply.github.com>
2025-12-27 17:41:01 +0530

    update SettingsExperimentalScreen and SettingsTroubleshootingScreen with preference elements

--- app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsExperimentalScreen.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsExperimentalScreen.kt
@@ -2,9 +2,13 @@ package com.nutomic.syncthingandroid.settings
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.navigation3.runtime.EntryProviderScope
 import com.nutomic.syncthingandroid.R
+import me.zhanghai.compose.preference.SwitchPreference
+import me.zhanghai.compose.preference.TextFieldPreference
 
 
 fun EntryProviderScope<SettingsRoute>.settingsExperimentalEntry() {
@@ -16,9 +20,39 @@ fun EntryProviderScope<SettingsRoute>.settingsExperimentalEntry() {
 
 @Composable
 fun SettingsExperimentalScreen() {
+    val useTor = remember { mutableStateOf(false) }
+    val socksProxy = remember { mutableStateOf("") }
+    val httpProxy = remember { mutableStateOf("") }
+
     SettingsScaffold(
         title = stringResource(R.string.category_experimental),
     ) {
-        Text(stringResource(R.string.category_experimental))
+        SwitchPreference(
+            title = { Text(stringResource(R.string.use_tor_title)) },
+            summary = { Text(stringResource(R.string.use_tor_summary)) },
+            state = useTor,
+        )
+
+        val socksProxySummary = if (socksProxy.value.isBlank())
+            "${stringResource(R.string.do_not_use_proxy)} ${stringResource(R.string.generic_example)}: ${stringResource(R.string.socks_proxy_address_example)}"
+        else
+            "${stringResource(R.string.use_proxy)} ${socksProxy.value}"
+        TextFieldPreference(
+            title = { Text(stringResource(R.string.socks_proxy_address_title)) },
+            summary = { Text(socksProxySummary) },
+            state = socksProxy,
+            textToValue = { it },
+        )
+
+        val httpProxySummary = if (httpProxy.value.isBlank())
+            "${stringResource(R.string.do_not_use_proxy)} ${stringResource(R.string.generic_example)}: ${stringResource(R.string.http_proxy_address_example)}"
+        else
+            "${stringResource(R.string.use_proxy)} ${httpProxy.value}"
+        TextFieldPreference(
+            title = { Text(stringResource(R.string.http_proxy_address_title)) },
+            summary = { Text(httpProxySummary) },
+            state = httpProxy,
+            textToValue = { it },
+        )
     }
 }
--- app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsTroubleShootingScreen.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsTroubleShootingScreen.kt
@@ -1,10 +1,20 @@
 package com.nutomic.syncthingandroid.settings
 
+import android.content.Intent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.navigation3.runtime.EntryProviderScope
 import com.nutomic.syncthingandroid.R
+import com.nutomic.syncthingandroid.activities.LogActivity
+import me.zhanghai.compose.preference.MultiSelectListPreference
+import me.zhanghai.compose.preference.Preference
+import me.zhanghai.compose.preference.SwitchPreference
+import me.zhanghai.compose.preference.TextFieldPreference
 
 
 fun EntryProviderScope<SettingsRoute>.settingsTroubleshootingEntry() {
@@ -16,9 +26,58 @@ fun EntryProviderScope<SettingsRoute>.settingsTroubleshootingEntry() {
 
 @Composable
 fun SettingsTroubleshootingScreen() {
+    val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
+
+    val issueTrackerUrl = stringResource(R.string.issue_tracker_url)
+
+    val verboseLog = remember { mutableStateOf(false) }
+    val stTraceOptions = remember { mutableStateOf(setOf<String>()) }
+    val envVars = remember { mutableStateOf("") }
+
     SettingsScaffold(
         title = stringResource(R.string.category_debug),
     ) {
-        Text(stringResource(R.string.category_debug))
+        Preference(
+            title = { Text(stringResource(R.string.report_issue_title)) },
+            summary = { Text(stringResource(R.string.open_issue_tracker_summary, issueTrackerUrl)) },
+            onClick = { uriHandler.openUri(issueTrackerUrl) }
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.verbose_log_title)) },
+            summary = { Text(stringResource(R.string.verbose_log_summary)) },
+            state = verboseLog,
+        )
+        Preference(
+            title = { Text(stringResource(R.string.open_log)) },
+            summary = { Text(stringResource(R.string.open_log_summary)) },
+            onClick = {
+                val intent = Intent(context, LogActivity::class.java)
+                context.startActivity(intent)
+            }
+        )
+        MultiSelectListPreference(
+            title = { Text(stringResource(R.string.sttrace_title)) },
+            state = stTraceOptions,
+            // TODO: values according to com.nutomic.syncthingandroid.views.SttracePreference.getDebugFacilities
+            values = listOf("beacon", "config", "connections", "db", "dialer", "discover"),
+        )
+        TextFieldPreference(
+            title = { Text(stringResource(R.string.environment_variables)) },
+            state = envVars,
+            textToValue = { it }
+        )
+        Preference(
+            title = { Text(stringResource(R.string.st_reset_database_title)) },
+            onClick = {
+                // TODO: show alert and call reset database
+            }
+        )
+        Preference(
+            title = { Text(stringResource(R.string.st_reset_deltas_title)) },
+            onClick = {
+                // TODO: show alert and call reset deltas
+            }
+        )
     }
 }
