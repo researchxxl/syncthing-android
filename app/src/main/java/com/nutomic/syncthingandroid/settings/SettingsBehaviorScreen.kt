31f9e49b0478a97d5a8badf6c115e9314db700da
Dhruv Bhavsar <40565844+dbhavsar76@users.noreply.github.com>
2025-12-27 12:17:06 +0530

    update SettingsBehaviorScreen with preference elements

--- app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsBehaviorScreen.kt
+++ app/src/main/java/com/nutomic/syncthingandroid/settings/SettingsBehaviorScreen.kt
@@ -2,9 +2,12 @@ package com.nutomic.syncthingandroid.settings
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import androidx.navigation3.runtime.EntryProviderScope
 import com.nutomic.syncthingandroid.R
+import me.zhanghai.compose.preference.SwitchPreference
 
 
 fun EntryProviderScope<SettingsRoute>.settingsBehaviorEntry() {
@@ -15,10 +18,29 @@ fun EntryProviderScope<SettingsRoute>.settingsBehaviorEntry() {
 
 
 @Composable
-fun SettingsBehaviorScreen(onBack: () -> Unit = {}) {
+fun SettingsBehaviorScreen() {
+    // temp code to make it work
+    val autoStart = remember { mutableStateOf(false) }
+    val broadcast = remember { mutableStateOf(false) }
+    val overwrite = remember { mutableStateOf(false) }
+
     SettingsScaffold(
         title = stringResource(R.string.category_behaviour),
     ) {
-        Text(stringResource(R.string.category_behaviour))
+        SwitchPreference(
+            title = { Text(stringResource(R.string.behaviour_autostart_title)) },
+            summary = { Text(stringResource(R.string.behaviour_autostart_summary)) },
+            state = autoStart,
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.broadcast_service_control_title))},
+            summary = { Text(stringResource(R.string.broadcast_service_control_summary))},
+            state = broadcast,
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.allow_overwrite_files_title)) },
+            summary = { Text(stringResource(R.string.allow_overwrite_files_summary))},
+            state = overwrite,
+        )
     }
 }
