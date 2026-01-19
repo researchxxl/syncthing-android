24cd869a1e179aa4c161410bd6ca6a266733ec49
researchxxl <244199422+researchxxl@users.noreply.github.com>
2026-01-19 12:56:12 +0100

    remove kotlin dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.aboutLibraries) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
 }
 
