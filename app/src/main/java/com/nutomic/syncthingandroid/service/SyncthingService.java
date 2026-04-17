b89978f32c49b9bfcbb410eedab493d3b1c0f5b8
Dhruv Bhavsar <40565844+dbhavsar76@users.noreply.github.com>
2026-04-17 21:57:56 +0530

    fix error importing or exporting config with empty string as backup path

--- app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
+++ app/src/main/java/com/nutomic/syncthingandroid/service/SyncthingService.java
@@ -798,11 +798,12 @@ public class SyncthingService extends Service {
      * Get backup zip file.
      * Default: /storage/emulated0/backups/syncthing/config.zip
      */
-    private final File getBackupZipFile() {
-        String relPathToZip = mPreferences.getString(
-                Constants.PREF_BACKUP_REL_PATH_TO_ZIP,
-                "backups/syncthing/config.zip"
-        );
+    private File getBackupZipFile() {
+        String defaultPath = "backups/syncthing/config.zip";
+        String relPathToZip = mPreferences.getString(Constants.PREF_BACKUP_REL_PATH_TO_ZIP, defaultPath);
+        if (relPathToZip.isEmpty()) {
+            relPathToZip = defaultPath;
+        }
         return new File(Environment.getExternalStorageDirectory(), relPathToZip);
     }
 
